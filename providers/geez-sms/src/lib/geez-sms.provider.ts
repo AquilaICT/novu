3567c10707638c3582a0193a2b66ce9cb6f7961c
ba5liel <baslielselamu2018@gmail.com>
2024-06-19 09:44:04 +0300

    geez updated

--- providers/geez-sms/src/lib/geez-sms.provider.ts
+++ providers/geez-sms/src/lib/geez-sms.provider.ts
@@ -4,26 +4,33 @@ import {
   ISmsOptions,
   ISmsProvider,
 } from '@novu/stateless';
-
+import { GeezSMS } from 'geezsms';
 export class GeezSmsSmsProvider implements ISmsProvider {
   id = 'geez-sms';
   channelType = ChannelTypeEnum.SMS as ChannelTypeEnum.SMS;
+  private geezSMS: GeezSMS;
 
   constructor(
     private config: {
-      
+      token: string;
+      sender_id?: number;
     }
   ) {
+    this.geezSMS = new GeezSMS(config.token)
   }
 
   async sendMessage(
     options: ISmsOptions
   ): Promise<ISendMessageSuccessResponse> {
-
-
+    const message = {
+      phone: options.to,
+      msg: options.msg,
+      sender_id: options.sender_id || this.config.sender_id  // Optional
+    };
+    const response = await this.geezSMS.single.send(message);
     return {
-      id: 'id_returned_by_provider',
-      date: 'current_time'
+      id: response.data.api_log_id,
+      date: response.data.date
     };
   }
 }
